30dcf1fc874c6bca2d270cea38eb33a5cdb66fff
Mateus Pinho <mateus.pinho@rnl.tecnico.ulisboa.pt>
2024-05-14 16:09:44 +0100

    hsafdhsqhds

--- hanabi.py
+++ hanabi.py
@@ -24,20 +24,3 @@ for agent in env.agent_iter():
 
     env.step(action)
 env.close()
-
-
-env = hanabi_v5.env(render_mode="human")
-env.reset(seed=42)
-
-for agent in env.agent_iter():
-    observation, reward, termination, truncation, info = env.last()
-
-    if termination or truncation:
-        action = None
-    else:
-        mask = observation["action_mask"]
-        # this is where you would insert your policy
-        action = env.action_space(agent).sample(mask)
-
-    env.step(action)
-env.close()
