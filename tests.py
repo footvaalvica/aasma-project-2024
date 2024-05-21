e2d71e89182c93b5d3cb1dd6ef2da6be3236a789
Martim Monis <martim.monis@rnl.tecnico.ulisboa.pt>
2024-05-21 21:57:52 +0100

    wip tests

--- tests.py
+++ tests.py
@@ -26,11 +26,38 @@ def find_subclasses(module, clazz, blacklist=[]):
 blacklist = ['MCTS', 'PlayerInput', 'Policy'] # Policies NOT to run!!
 policy_classes = find_subclasses(sys.modules['policies'], Policy, blacklist)
 
+# Function to run a match between two policies given a seed
+def run_match(policy1, policy2, seed):
+    print(f"Running match between {policy1.__name__} and {policy2.__name__} with seed {seed}")
+    # Run the game
+    env = EnvWrapper(colors=5, ranks=5, players=2, hand_size=5, max_information_tokens=8, max_life_tokens=3, observation_type='card_knowledge', render_mode='human')
+    env.reset(seed=seed)
+    env.render()
+    cards_age = {}
+    for i in range(env.num_agents):
+        cards_age_key = f"player_{i}"
+        cards_age[cards_age_key] = [0, 0, 0, 0, 0]
+    for agent in env.agent_iter():
+        observation, reward, termination, truncation, info = env.last()
+        card_age = cards_age[agent]
+        if termination or truncation:
+            action = None
+        else:
+            mask = observation["action_mask"]
+            obs = observation["observation"]
+            if agent == "player_0":
+                policy = policy1(env, agent, mask, obs, card_age)
+            else:
+                policy = policy2(env, agent, mask, obs, card_age)
+            action = policy.run()
+            print(f"Agent {agent} chose action {action}")
+            env.action_history.append(action)
+            cards_age[agent] = update_card_age(card_age, action)
+        env.step(action)
+
 # Get each pair combination of agents (policies)
 for i in range(len(policy_classes)):
     for j in range(i+1, len(policy_classes)):
         for seed in seeds:
-            print(f"python hanabi.py {policy_classes[i].__name__} {policy_classes[j].__name__} {seed}")
-
-# Run each pair combination of agents with the same configurations
+            run_match(policy_classes[i], policy_classes[j], seed)
 
